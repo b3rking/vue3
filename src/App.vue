9f7f3335f069415e31baf2f679b73b86b0f14ded
b3rking <ishimweeschylebersy@gmail.com>
2023-02-20 16:15:55 +0200

    feat: v-html, v-if, v-else, v-else-if

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
     <!-- this is used to print out simple text -->
-    <h1>{{ title }}</h1>
+    <h1 v-if="isVisible">{{ title }}</h1>
+    <!-- v-else-if -->
+    <h1 v-else-if="isVisible === null">v-else-if {{ title }}</h1>
+    <!-- using v-else -->
+    <h1 v-else>if not visible try : {{ title }}</h1>
     <!-- this is used to print html -->
     <em v-html="secondTitle"></em>
+    <!-- v-text -->
 </template>
 
 <script>
@@ -11,6 +16,7 @@ export default {
         return {
             title: "hii to vue3",
             secondTitle: "<h1>title written with html tag</h1>",
+            isVisible: null,
         };
     },
 };
