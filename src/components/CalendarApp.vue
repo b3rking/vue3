cd1aa6db823021c4b64fc69b61384460894c3e21
b3rking <ishimweeschylebersy@gmail.com>
2023-02-23 17:15:26 +0200

    feat: showing days in calendar

--- src/components/CalendarApp.vue
+++ src/components/CalendarApp.vue
@@ -1,11 +1,24 @@
 <template>
-    <div class="text-center center">
-        <h1>calendar</h1>
+    <div class="text-center center w-2/5 mx-auto my-6">
+        <h1 class="text-2xl font-bold capitalize">calendar</h1>
+    
+        <section class="flex flex-row items-center justify-between my-3">
+            <p v-for="day in days" :key="day" class="w-10 h-10">{{  day }}</p>
+        </section>
+        <section class="flex">
+            <p v-for="num in 30" :key="num" class="text-center p-4">{{  num }}</p>
+        </section>
     </div>
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+        }
+    }
+};
 </script>
 
 <style></style>
