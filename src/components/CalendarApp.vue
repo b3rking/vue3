607fcf8b82e54cc4120868d2507d7c50107b912d
b3rking <ishimweeschylebersy@gmail.com>
2023-02-27 11:53:28 +0200

    feat: showing the exact number of month

--- src/components/CalendarApp.vue
+++ src/components/CalendarApp.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="text-center center w-2/5 mx-auto my-6">
         <h1 class="text-2xl font-bold capitalize">calendar</h1>
-        <h2 class="my-2 capitalize font-semibold">{{ currentMonth }}</h2>
+        <h2 class="my-2 capitalize font-semibold">{{ currentMonth }} - {{ currentYear }}</h2>
         <section class="flex flex-row items-center justify-between my-3 mt-6">
             <p
                 v-for="day in days"
@@ -14,7 +14,7 @@
         </section>
         <section class="flex flex-wrap">
             <p
-                v-for="num in 30"
+                v-for="num in daysInMonth()"
                 :key="num"
                 class="text-center"
                 style="width: 14.28%"
@@ -27,12 +27,19 @@
 
 <script>
 export default {
+    methods: {
+        daysInMonth() {
+            const month = new Date().getMonth() + 1
+            return new Date(this.currentYear, month, 0).getDate()
+        }
+    },
     data() {
         return {
             currentMonth: new Date().toLocaleString(
                 "default",
                 { month: "long" }
             ),
+            currentYear: new Date().getFullYear(),
             days: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
         };
     },
