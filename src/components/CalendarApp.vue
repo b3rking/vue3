c8f0bc9aad83502a30c829523211b300b6f55117
b3rking <ishimweeschylebersy@gmail.com>
2023-02-27 11:39:58 +0200

    feat: shwoing the actual month

--- src/components/CalendarApp.vue
+++ src/components/CalendarApp.vue
@@ -1,12 +1,26 @@
 <template>
     <div class="text-center center w-2/5 mx-auto my-6">
         <h1 class="text-2xl font-bold capitalize">calendar</h1>
-    
+        <h2 class="my-2 capitalize font-semibold">{{ currentMonth }}</h2>
         <section class="flex flex-row items-center justify-between my-3 mt-6">
-            <p v-for="day in days" :key="day" class="font-bold" style="width: 14.28%;">{{  day }}</p>
+            <p
+                v-for="day in days"
+                :key="day"
+                class="font-bold"
+                style="width: 14.28%"
+            >
+                {{ day }}
+            </p>
         </section>
         <section class="flex flex-wrap">
-            <p v-for="num in 30" :key="num" class="text-center" style="width: 14.28%;">{{  num }}</p>
+            <p
+                v-for="num in 30"
+                :key="num"
+                class="text-center"
+                style="width: 14.28%"
+            >
+                {{ num }}
+            </p>
         </section>
     </div>
 </template>
@@ -15,9 +29,13 @@
 export default {
     data() {
         return {
-            days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-        }
-    }
+            currentMonth: new Date().toLocaleString(
+                "default",
+                { month: "long" }
+            ),
+            days: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+        };
+    },
 };
 </script>
 
