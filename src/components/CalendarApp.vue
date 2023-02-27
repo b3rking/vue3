f93d46e6ecca85b34d99ae2cc947cfde25cf8aa1
b3rking <ishimweeschylebersy@gmail.com>
2023-02-27 12:13:45 +0200

    feat: showing the right starting day of the month

--- src/components/CalendarApp.vue
+++ src/components/CalendarApp.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="text-center center w-2/5 mx-auto my-6">
         <h1 class="text-2xl font-bold capitalize">calendar</h1>
-        <h2 class="my-2 capitalize font-semibold">{{ currentMonth }} - {{ currentYear }}</h2>
+        <h2 class="my-2 capitalize font-semibold">
+            {{ currentMonth }} - {{ currentYear }}
+        </h2>
         <section class="flex flex-row items-center justify-between my-3 mt-6">
             <p
                 v-for="day in days"
@@ -13,6 +15,13 @@
             </p>
         </section>
         <section class="flex flex-wrap">
+            <p
+                v-for="num in startDay()"
+                :key="num"
+                class="text-center"
+                style="width: 14.28%"
+            ></p>
+
             <p
                 v-for="num in daysInMonth()"
                 :key="num"
@@ -29,16 +38,19 @@
 export default {
     methods: {
         daysInMonth() {
-            const month = new Date().getMonth() + 1
-            return new Date(this.currentYear, month, 0).getDate()
-        }
+            const month = this.month;
+            return new Date(this.currentYear, month, 0).getDate();
+        },
+        startDay() {
+            return new Date(this.currentYear, this.month - 1, 1).getDay();
+        },
     },
     data() {
         return {
-            currentMonth: new Date().toLocaleString(
-                "default",
-                { month: "long" }
-            ),
+            currentMonth: new Date().toLocaleString("default", {
+                month: "long",
+            }),
+            month: new Date().getMonth() + 1,
             currentYear: new Date().getFullYear(),
             days: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
         };
