e7d140c518d3a8334353de79cc6b194498645f75
b3rking <ishimweeschylebersy@gmail.com>
2023-02-27 11:33:42 +0200

    feat: add styling for the calendar

--- src/components/CalendarApp.vue
+++ src/components/CalendarApp.vue
@@ -2,11 +2,11 @@
     <div class="text-center center w-2/5 mx-auto my-6">
         <h1 class="text-2xl font-bold capitalize">calendar</h1>
     
-        <section class="flex flex-row items-center justify-between my-3">
-            <p v-for="day in days" :key="day" class="w-10 h-10">{{  day }}</p>
+        <section class="flex flex-row items-center justify-between my-3 mt-6">
+            <p v-for="day in days" :key="day" class="font-bold" style="width: 14.28%;">{{  day }}</p>
         </section>
-        <section class="flex">
-            <p v-for="num in 30" :key="num" class="text-center p-4">{{  num }}</p>
+        <section class="flex flex-wrap">
+            <p v-for="num in 30" :key="num" class="text-center" style="width: 14.28%;">{{  num }}</p>
         </section>
     </div>
 </template>
