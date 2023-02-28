d3619a8c4d9e9ade1d87edbf15228edc2fecabd5
b3rking <ishimweeschylebersy@gmail.com>
2023-02-28 13:54:20 +0200

    feat: add markdown app

--- src/pages/MarkdownApp.vue
+++ src/pages/MarkdownApp.vue
@@ -1,9 +1,27 @@
 <template>
-    <div class="w-2/5 mx-auto">
-        Markdown
+    <div class="text-center">
+        <h1 class="my-2 text-xl font-semibold">Markdown</h1>
     </div>
+    <section class="flex w-full h-screen px-20">
+        <article class="w-1/2 border">
+            <textarea class="w-full h-screen" v-model="text"></textarea>
+        </article>
+        <article class="w-1/2 border bg-gray-100">{{ markedText }}</article>
+    </section>
 </template>
 <script>
-export default {}
+import marked from 'marked';
+export default {
+    data() {
+        return {
+            text: "",
+        };
+    },
+    computed: {
+        markedText() {
+            return marked(this.text)
+        }
+    }
+};
 </script>
 <style></style>
