b457892618d1126f597fd5c2f40ffd39f52d3548
b3rking <ishimweeschylebersy@gmail.com>
2023-03-12 15:46:42 +0200

    feat: design the login form

--- src/components/LoginModal.vue
+++ src/components/LoginModal.vue
@@ -1,15 +1,51 @@
 <template>
     <div
         class="absolute top-0 left-0 h-screen w-screen z-2"
-        style="background-color: rgba(0, 0, 0, 0.7)"
-        @click="$emit('hide-modal')"
-    >
-        <div class="bg-white flex m-auto w-1/3 my-40 rounded-md p-3">
-            <div class="">login</div>
+        style="background-color: rgba(0, 0, 0, 0.7)">
+        <div class="bg-white flex m-auto w-1/2 my-40 rounded-md p-3">
+            <div class="text-xl text-center font-semibold w-full">
+                <h1 class="text-center capitalize">login</h1>
+                <hr class="my-3" />
+                <form @submit.prevent="submit">
+                    <div class="m-3">
+                        <input
+                            class="border-2 border-gray-600 rounded-md py-2 px-4"
+                            v-model="form.email"
+                            type="text"
+                            placeholder="email" />
+                    </div>
+                    <div class="m-3">
+                        <input
+                            class="border-2 border-gray-600 rounded-md py-2 px-4"
+                            v-model="form.password"
+                            type="password"
+                            placeholder="password" />
+                    </div>
+                    <button
+                        type="submit"
+                        class="bg-slate-900 py-2 px-5 rounded-md text-white">
+                        Sign In
+                    </button>
+                </form>
+            </div>
         </div>
     </div>
 </template>
 <script>
-export default {}
+export default {
+    data() {
+        return {
+            form: {
+                email: "",
+                password: "",
+            }
+        };
+    },
+    methods: {
+        submit() {
+            console.log(this.form)
+        }
+    }
+};
 </script>
 <style></style>
