57bb3635328bafe3de42706d41133fddb33584e5
b3rking <ishimweeschylebersy@gmail.com>
2023-03-18 10:22:21 +0200

    feat: add calculator design

--- src/pages/CalculatorApp.vue
+++ src/pages/CalculatorApp.vue
@@ -1,5 +1,22 @@
 <template>
-    calculator
+    <section class="flex w-full">
+        <div class="m-auto">
+            <h1 class="text-2xl capitalize text-center">calculator</h1>
+            <div class="my-10 grid grid-cols-3 gap-1">
+                <button class="p-2 w-10 h-4border rounded shadow">1</button>
+                <button class="p-2 w-10 h-4border rounded shadow">2</button>
+                <button class="p-2 w-10 h-4border rounded shadow">3</button>
+                <button class="p-2 w-10 h-4border rounded shadow">4</button>
+                <button class="p-2 w-10 h-4border rounded shadow">5</button>
+                <button class="p-2 w-10 h-4border rounded shadow">6</button>
+                <button class="p-2 w-10 h-4border rounded shadow">7</button>
+                <button class="p-2 w-10 h-4border rounded shadow">8</button>
+                <button class="p-2 w-10 h-4border rounded shadow">9</button>
+                <button class="p-2 h-4border rounded shadow col-span-2">0</button>
+                <button class="p-2 w-10 h-4border rounded shadow"> = </button>
+            </div>
+        </div>
+    </section>
 </template>
 <script></script>
 <style></style>
